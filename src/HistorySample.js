b801fd9d1c504bdff717c23073b1ea9e9f54852f
song <songnumber1@naver.com>
2024-01-18 20:54:47 +0900

    히스토리 샘플 수정

--- src/HistorySample.js
+++ src/HistorySample.js
@@ -1,38 +1,22 @@
 import React from "react";
 // import React, { useEffect } from "react";
-// import { useHistory } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 
 const HistorySample = () => {
-  // const history = useHistory();
+  const history = useNavigate();
 
-  //   const goBack = () => {
-  //     history.goBack();
-  //   };
+  const goBack = () => {
+    history.goBack();
+  };
 
-  //   const goHome = () => {
-  //     history.push("/");
-  //   };
+  const goHome = () => {
+    history.push("/");
+  };
 
-  //   useEffect(() => {
-  //     console.log(history);
-  //     const unblock = history.block("정말 떠나실건가요?");
-
-  //     return () => {
-  //       unblock();
-  //     };
-  //   }, [history]);
-
-  //   return (
-  //     <div>
-  //       <button onClick={goBack}>뒤로가기</button>
-  //       <button onClick={goHome}>홈으로</button>
-  //     </div>
-  //   );
   return (
     <div>
-      <div>로그인 페이지입니다.</div>
-      <button>로그인</button>
-      <button>뒤로가기</button>
+      <button onClick={goBack}>뒤로가기</button>
+      <button onClick={goHome}>홈으로</button>
     </div>
   );
 };
